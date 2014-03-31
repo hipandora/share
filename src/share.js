cf0da5b818404fecef4bcb70b17d319aac2068b6
FortuneZhang <zhangfortune527@gmail.com>
2014-03-31 16:37:32 +0800

    fortune- render total share count

--- src/share.js
+++ src/share.js
@@ -52,8 +52,11 @@
         var partial_title = '#' + $('.share-note-type').text().trim() + '#：' + $(".share-title").text().trim() + '［' + $('.share-note-day-count').text().trim() + '天/' + $('.img-box').length + '图］，'
 
         var pic_anchor = location.href;
+
+        var is_share_article = true ;
         if (this.children('img').attr('id') != undefined) {
             pic_anchor = location.href.split('#')[0] + '#' + this.children('img').attr('id');
+            is_share_article = false ;
         }
 
         if ($($('.current-user-id')[0]).text().trim() == $('.note-author').text().trim()) {
@@ -62,7 +65,7 @@
             title += '我发现了一篇很实用的' + partial_title + '并分享给大家！' + pic_anchor + '（分享自@hi潘多拉网 ）';
         }
         var param = {
-            url: location.href.split('#')[0],
+            url: location.href.split('#')[0] + (is_share_article ? '' : '#the_user_item'),
             type: '1',
             count: '',
             appkey: '1153655536',
@@ -104,8 +107,6 @@
             '</div>';
         if (option.share_with_status) {
             $(share_with_status).appendTo(this).css('position', 'fixed').css('top', $(window).height() / 3).css('left', $('.white-bg').offset().left - 40).show();
-            render_sina_share_counts();
-
         } else {
             $(only_share_content).appendTo(this);
         }
@@ -116,7 +117,9 @@
         $(this.parent('.content-sub-box.preview-content-sub-box')).mouseleave(function () {
             $(this).find('.share-pic-container').css('display', 'none');
         });
-
+        return this;
+    };
+    $.render_share_count = function(){
         function render_sina_share_counts() {
             $.ajax({
                 url: 'https://api.weibo.com/2/short_url/shorten.json?url_long=' + encodeURIComponent(location.href.split('#')[0]) + '&access_token=2.00JT793DVzTAUE1549e8542b3w2R8E',
@@ -128,7 +131,9 @@
                         type: "GET",
                         dataType: "jsonp",
                         success: function (share_count_result, textStatus, xhr) {
-                            $('.sina-share-count').text(share_count_result['data']['urls'][0]['share_counts']);
+                            var article_share_count = share_count_result['data']['urls'][0]['share_counts'];
+                            $('.sina-share-count').text(article_share_count);
+                            $.cookie('article_share_count', article_share_count,{ path: "/"});
                         },
                         error: function (error) {
                             console.log('获取分享数错误');
@@ -142,7 +147,36 @@
             })
         }
 
-        return this;
-    };
+        function render_total_share_counts(){
+            $.ajax({
+                url: 'https://api.weibo.com/2/short_url/shorten.json?url_long=' + encodeURIComponent(location.href.split('#')[0] + '#the_user_item') + '&access_token=2.00JT793DVzTAUE1549e8542b3w2R8E',
+                type: "GET",
+                dataType: "jsonp",
+                success: function (short_url_result, textStatus, xhr) {
+                    $.ajax({
+                        url: 'https://api.weibo.com/2/short_url/share/counts.json?url_short=' + short_url_result['data']['urls'][0]['url_short'] + '&access_token=2.00JT793DVzTAUE1549e8542b3w2R8E',
+                        type: "GET",
+                        dataType: "jsonp",
+                        success: function (share_count_result, textStatus, xhr) {
+                            var pic_share_count = share_count_result['data']['urls'][0]['share_counts'];
+                            $('.share-total-count').text(parseInt(pic_share_count) + parseInt($.cookie('article_share_count')));
+                        },
+                        error: function (error) {
+                            console.log('获取分享数错误');
+                        }
+
+                    });
+                },
+                error: function (error) {
+                    console.log('获取短网址错误');
+                }
+            });
+        }
+
+        render_sina_share_counts();
+        render_total_share_counts();
+
+    }
+
 
 }(jQuery));
