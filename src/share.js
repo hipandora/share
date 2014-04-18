8c7e6554a417feef206847659fe9bc272a5b7910
vamdt <liang6688267@gmail.com>
2014-04-18 11:19:23 +0800

    Delete aswsome

--- src/share.js
+++ src/share.js
@@ -8,34 +8,6 @@
 
 (function ($) {
 
-    // Collection method.
-    $.fn.awesome = function () {
-        return this.each(function (i) {
-            // Do something awesome to each selected element.
-            $(this).html('awesome' + i);
-        });
-    };
-
-    // Static method.
-    $.awesome = function (options) {
-        // Override default options with passed-in options.
-        options = $.extend({}, $.awesome.options, options);
-        // Return something awesome.
-        return 'awesome' + options.punctuation;
-    };
-
-    // Static method default options.
-    $.awesome.options = {
-        punctuation: '.'
-    };
-
-    // Custom selector.
-    $.expr[':'].awesome = function (elem) {
-        // Is this element awesome?
-        return $(elem).text().indexOf('awesome') !== -1;
-    };
-
-
     $.share_url = function(){
         return location.origin + '/index/viewNote?id=' + location.href.split('#')[0].split('=')[1];
     }
